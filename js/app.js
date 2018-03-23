40716b61d5c693b2da2479fb8b5e925c9bfa2309
Dominika Porzyczka <dominikaporzyczka@gmail.com>
2018-03-23 14:57:04 +0100

    Refactor constructors

--- js/app.js
+++ js/app.js
@@ -1,72 +1,77 @@
 // Enemies our player must avoid
 let allEnemies = [];
 
-var Enemy = function () {
-    this.position = [60, 140, 220];
-    this.x = -100;
-    this.y = this.position[Math.floor(Math.random() * this.position.length)];
-    this.speed = (Math.random() * 4) + 2;
+class Enemy {
+    constructor() {
+        this.position = [60, 140, 220];
+        this.x = -100;
+        this.y = this.position[Math.floor(Math.random() * this.position.length)];
+        this.speed = (Math.random() * 4) + 2;
+    
+        this.sprite = 'images/enemy-bug.png';
+        this.width = 101;
+        this.height = 171;
+    }
 
-    this.sprite = 'images/enemy-bug.png';
-    this.width = 101;
-    this.height = 171;
-};
+    // Update the enemy's position
+    // Parameter: dt, a time delta between ticks
+    update(dt) {
+        this.x += this.speed;
+    }
 
-// Update the enemy's position
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function (dt) {
-    this.x += this.speed;
-};
-
-// Draw the enemy on the screen
-Enemy.prototype.render = function () {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
-const Player = function () {
-    this.x = 200;
-    this.y = 380;
-    this.sprite = 'images/char-cat-girl.png';
-    this.width = 101;
-    this.height = 171;
-}
-
-Player.prototype.moveToDefault = function () {
-    this.x = 200;
-    this.y = 380;
-}
-
-// Update the player position, add message when win
-Player.prototype.update = function () {
-    if (this.y < 60) {
-        allEnemies = [];
-        this.moveToDefault();
-
-        const messages = ['Good job!', 'Nice!', 'You are awesome!', 'Woooooo!', 'You win!!!'];
-        this.messageParagraph = document.querySelector('.message');
-        this.messageParagraph.textContent = messages[Math.floor(Math.random() * messages.length)];
+    // Draw the enemy on the screen
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
 }
 
-Player.prototype.handleInput = function (key) {
-    if (key === 'left' && this.x >= 100) {
-        this.x -= 100;
+class Player {
+    constructor() {
+        this.x = 200;
+        this.y = 380;
+        this.sprite = 'images/char-cat-girl.png';
+        this.width = 101;
+        this.height = 171;
     }
-    else if (key === 'up' && this.y >= 60) {
-        this.y -= 80;
-    }
-    else if (key === 'right' && this.x <= 300) {
-        this.x += 100;
-    }
-    else if (key === 'down' && this.y <= 300) {
-        this.y += 80;
-    }
-}
 
-// Draw the player on the screen
-Player.prototype.render = function () {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+    moveToDefault() {
+        this.x = 200;
+        this.y = 380;
+    }
+
+    // Update the player position, add message when win
+    update() {
+        if (this.y < 60) {
+            allEnemies = [];
+            this.moveToDefault();
+
+            const messages = ['Good job!', 'Nice!', 'You are awesome!', 'Woooooo!', 'You win!!!'];
+            this.messageParagraph = document.querySelector('.message');
+            this.messageParagraph.textContent = messages[Math.floor(Math.random() * messages.length)];
+        }
+    }
+
+    handleInput(key) {
+        if (key === 'left' && this.x >= 100) {
+            this.x -= 100;
+        }
+        else if (key === 'up' && this.y >= 60) {
+            this.y -= 80;
+        }
+        else if (key === 'right' && this.x <= 300) {
+            this.x += 100;
+        }
+        else if (key === 'down' && this.y <= 300) {
+            this.y += 80;
+        }
+    }
+
+    // Draw the player on the screen
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+
+}
 
 // Add new enemies
 allEnemies = [new Enemy()];
